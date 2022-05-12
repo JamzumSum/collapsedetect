64585c11ed01555afd4a5e962d02580317bee705
JamzumSum <zzzzss990315@gmail.com>
2022-05-12 15:57:32 +0800

    docs: add sphinx.ext.githubpages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ release = "0.1.0"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
